514b7f734e8eb5f59130e712bb6a77ffa7383b98
ybenadda <ybenadda@youcan.shop>
2022-11-07 16:30:28 +0100

    DOT-6741 > YC-CLI: Add WriteToFile and homeDir utils

--- lib/commands/login/index.ts
+++ lib/commands/login/index.ts
@@ -1,7 +1,4 @@
-import openLink from '../../utils/openLink';
-
-const authorizationUrl = 'https://seller-area.youcan.shop/admin/oauth/authorize?client_id=1&redirect_uri=http://localhost:1337/callback&response_type=code&scope=*';
-
+// Todo: Implement login command
 export default function loginCommand() {
-  openLink(authorizationUrl);
+
 }
---
+++ lib/utils/writeToFile.ts
@@ -0,0 +1,19 @@
+import fs from 'fs';
+
+/**
+ * @param filePath - The path to the file to write to.
+ * @param content - The content to write to the file.
+ * @returns A promise that resolves when the file has been written.
+*/
+
+export default function writeToFile(filePath: string, content: string) {
+  return new Promise<void>((resolve, reject) => {
+    try {
+      fs.writeFileSync(filePath, content);
+      return resolve();
+    }
+    catch (error) {
+      return reject(error);
+    }
+  });
+}
