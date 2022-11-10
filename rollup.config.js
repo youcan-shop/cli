c029f2c9c24f839fb8adb5dfd9780eeba0799db8
Aymane D. Hlamnach <95413644+azuradarius@users.noreply.github.com>
2022-11-10 14:59:19 +0100

    build > switch to rollup, update build (#5)
    ## Notes
    
    - switched bundler to rollup + swc

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: 'lib/index.ts',
+  plugins: [
+    typescript({ tsconfig: 'tsconfig.json' }),
+    // dts({ tsconfig: 'tsconfig.json' }),
+  ],
+  output: {
+    dir: 'dist',
+    exports: 'named',
+    format: 'es',
+    preserveModules: true,
+  },
+};
