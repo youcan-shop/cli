8f161442e47be22636d96625b0b885a3a0732898
ayoubezzini <105205174+ayoubezzini@users.noreply.github.com>
2022-11-11 15:47:43 +0100

    HTTP Agent > Keep Alive (#10)

--- lib/utils/http.ts
+++ lib/utils/http.ts
@@ -1,15 +1,19 @@
+import { Agent } from 'http';
 import type { RequestInit } from 'node-fetch';
 import fetch from 'node-fetch';
 import { mergeDeepLeft } from 'ramda';
+
+const HttpAgent = new Agent({ keepAlive: true, keepAliveMsecs: 5 * 60 * 1000 });
 
 export const DEFAULT_HTTP_CLIENT_OPTIONS = {
   headers: {
     Accept: 'application/json',
   },
+  agent: HttpAgent,
 };
 
 async function request<T>(endpoint: string, options: RequestInit = {}): Promise<T> {
-  const response = await fetch(endpoint, mergeDeepLeft(options, DEFAULT_HTTP_CLIENT_OPTIONS));
+  const response = await fetch(endpoint, mergeDeepLeft(options, DEFAULT_HTTP_CLIENT_OPTIONS) as RequestInit);
   if (!response.ok)
     throw new Error(await response.text(), { cause: response });
 
