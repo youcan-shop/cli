a46f2e3f688a05b8dec713c3e611adf3af1963db
Aymane D. Hlamnach <aymane.hlamnach@youcan.shop>
2022-11-14 10:50:47 +0100

    dark > use https agent for keep-alive

--- lib/utils/http.ts
+++ lib/utils/http.ts
@@ -1,15 +1,15 @@
-import { Agent } from 'http';
+import { Agent } from 'https';
 import type { RequestInit } from 'node-fetch';
 import fetch from 'node-fetch';
 import { mergeDeepLeft } from 'ramda';
 
-const HttpAgent = new Agent({ keepAlive: true, keepAliveMsecs: 5 * 60 * 1000 });
+const HttpsAgent = new Agent({ keepAlive: true, keepAliveMsecs: 5 * 60 * 1000 });
 
 export const DEFAULT_HTTP_CLIENT_OPTIONS = {
   headers: {
     Accept: 'application/json',
   },
-  agent: HttpAgent,
+  agent: HttpsAgent,
 };
 
 async function request<T>(endpoint: string, options: RequestInit = {}): Promise<T> {
