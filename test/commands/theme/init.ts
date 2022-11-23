9bd98184a3a5d25039a47564dc4137db4e00d676
ybenadda <117163420+ybenadda@users.noreply.github.com>
2022-11-23 14:29:49 +0100

    DOT-6833 > Add tests

--- test/commands/theme/init.ts
+++ test/commands/theme/init.ts
@@ -4,11 +4,13 @@ import { expect, it } from 'vitest';
 
 export default () => {
   it('Should pull the starter theme in the current working directory', async () => {
-    if (fs.existsSync('./Starter'))
-      fs.rmdirSync('./Starter', { recursive: true });
-
     await execa('node', ['./dist/index.js', 'init', '--default']);
 
     expect(fs.existsSync('Starter')).toBe(true);
   });
+
+  it('Starter theme should have .youcan file', async () => {
+    expect(fs.existsSync('./Starter/.youcan')).toBe(true);
+  },
+  );
 };
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,22 @@
-import { describe } from 'vitest';
+import { existsSync, rmdirSync } from 'fs';
+import { describe, expect, it } from 'vitest';
 import loginTest from './commands/auth/login';
 import logoutTest from './commands/auth/logout';
-import initTest from './commands/commands/init';
+import initTest from './commands/theme/init';
 import helpTest from './commands/help';
+import devTest from './commands/theme/dev';
 
 describe('YouCan CLI tests', () => {
+  describe('initialization', () => {
+    it('remove the Starter theme', async () => {
+      if (existsSync('Starter'))
+        rmdirSync('Starter', { recursive: true });
+      expect(existsSync('Starter')).toBe(false);
+    });
+  });
   describe('--help command', helpTest);
   describe('auth:login command', loginTest);
   describe('theme:init command', initTest);
+  describe('theme:dev command', devTest);
   describe('auth:logout command', logoutTest);
 });
