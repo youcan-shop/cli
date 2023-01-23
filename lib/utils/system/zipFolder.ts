7b4606c57ef28d18ddb3926f442c65d689197ef6
Youssef <117163420+ybenadda@users.noreply.github.com>
2023-01-23 16:30:15 +0100

    feat: add package command (#30)
    ## JIRA Ticket
    
    [DOT-X](https://nextmediama.atlassian.net/browse/DOT-X)
    
    ## QA Steps
    
    * [ ] Go to X and Y
    * [ ] See TEXT in page Z
    
    ## Note
    
    Leave empty when you have nothing to say

--- lib/utils/system/zipFolder.ts
+++ lib/utils/system/zipFolder.ts
@@ -1,10 +1,11 @@
 import fs from 'fs';
-import path from 'path';
+import path, { resolve } from 'path';
 import archiver from 'archiver';
+import { lsDir } from './ls';
 /**
  * Zip folder and save it to a given path and return zip folder path
  */
-export default async function zipFolder(folderPath: string, folderName: string): Promise<string> {
+export async function zipFolder(folderPath: string, folderName: string): Promise<string> {
   return new Promise((resolve, reject) => {
     try {
       const zipPath = path.resolve(folderPath, `${folderName}.zip`);
@@ -22,6 +23,34 @@ export default async function zipFolder(folderPath: string, folderName: string):
 
       archive.pipe(output);
       archive.directory(path.resolve(folderPath, folderName), false);
+      archive.finalize();
+    }
+    catch (err) {
+      reject(err);
+    }
+  });
+}
+
+export async function zipDirectory(dirPath: string, folderName: string) {
+  const ls = lsDir(dirPath);
+
+  return new Promise((resolve, reject) => {
+    try {
+      const zip = path.resolve(dirPath, `${folderName}.zip`);
+      const output = fs.createWriteStream(zip);
+      const archive = archiver('zip', {
+        zlib: { level: 9 },
+      });
+
+      output.on('close', () => {
+        resolve(zip);
+      });
+      archive.on('error', (err: any) => {
+        reject(err);
+      });
+
+      archive.pipe(output);
+      ls.forEach(folder => archive.directory(path.resolve(dirPath, folder), folder));
       archive.finalize();
     }
     catch (err) {
