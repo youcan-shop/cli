9b1f3542aea3fca4dc86948366032e045e977e16
Youssef <117163420+ybenadda@users.noreply.github.com>
2023-01-27 17:22:20 +0100

    fix: pack only the theme required folders (#32)

--- lib/cli/commands/theme/pack.ts
+++ lib/cli/commands/theme/pack.ts
@@ -4,7 +4,7 @@ import stdout from '@/utils/system/stdout';
 import { LoadingSpinner, getCurrentDate, getCurrentThemeId } from '@/utils/common';
 import messages from '@/config/messages';
 import { zipDirectory } from '@/utils/system/zipFolder';
-import { lsDir } from '@/utils/system/ls';
+import config from '@/config';
 
 export default function command(cli: CLI): CommandDefinition {
   return {
@@ -24,7 +24,7 @@ export default function command(cli: CLI): CommandDefinition {
       loading.start();
 
       const exportFolder = `theme_${getCurrentDate()}`;
-      await zipDirectory(cwd(), exportFolder);
+      await zipDirectory(cwd(), exportFolder, config.THEME_FILE_TYPES);
       loading.stop();
 
       return stdout.info(`your theme was successfully packaged to ${exportFolder}`);
