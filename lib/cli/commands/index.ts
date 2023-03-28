52297766be1bbfd47dabc5ae38dd76da21258fbc
YaaY <105205174+ayoubezzini@users.noreply.github.com>
2023-03-28 12:39:34 +0000

    Cli > Add store info command /  list development stores command (#40)
    ## JIRA Ticket
    
    [DOT-7482](https://youcanshop.atlassian.net/browse/DOT-7482)
    [DOT-7483](https://youcanshop.atlassian.net/browse/DOT-7483)
    
    ## QA Steps
    
    * [ ] Use `youcan store:info` to show the current store info.
    * [ ] Use `youcan theme:list` to list the development  themes
    
    
    [DOT-7482]:
    https://youcanshop.atlassian.net/browse/DOT-7482?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    [DOT-7483]:
    https://youcanshop.atlassian.net/browse/DOT-7483?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- lib/cli/commands/index.ts
+++ lib/cli/commands/index.ts
@@ -4,10 +4,12 @@ export { default as LogoutCommand } from './auth/logout';
 
 // theme
 export { default as ThemeInitCommand } from './theme/init';
+export { default as ThemeListCommand } from './theme/list';
 export { default as ThemeDevCommand } from './theme/dev';
 export { default as ThemeDeleteCommand } from './theme/delete';
 export { default as ThemePullCommand } from './theme/pull';
 export { default as ThemePackCommand } from './theme/pack';
 
 // store
+export { default as StoreInfoCommand } from './store/info';
 export { default as StoreSwitchCommand } from './store/switch';
