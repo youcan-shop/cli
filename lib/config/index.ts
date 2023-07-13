357d7318ae69d4cc8de66881dbde92f92c67e689
abdohomrani <habdellah@youcan.shop>
2023-07-13 16:39:55 +0100

    DARK: Clone theme using https instead of ssh

--- lib/config/index.ts
+++ lib/config/index.ts
@@ -13,10 +13,10 @@ export default {
   SELLER_AREA_API_BASE_URI: 'https://api.youcan.shop',
   SELLER_AREA_WEB_BASE_URI: 'https://seller-area.youcan.shop',
 
-  STARTER_THEME_GIT_REPOSITORY: 'git@github.com:youcan-shop/light-theme.git',
+  STARTER_THEME_GIT_REPOSITORY: 'https://github.com/youcan-shop/light-theme.git',
   AVAILABLE_THEMES: [
-    { name: 'default', repository: 'git@github.com:youcan-shop/light-theme.git' },
-    { name: 'cod-theme', repository: 'git@github.com:youcan-shop/cod-theme.git' },
+    { name: 'default', repository: 'https://github.com/youcan-shop/light-theme.git' },
+    { name: 'cod-theme', repository: 'https://github.com/youcan-shop/cod-theme.git' },
   ],
 
   CLI_GLOBAL_CONFIG_DIR: path.resolve(homedir(), '.youcan'),
