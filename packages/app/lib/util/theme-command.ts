24b11548100360df7ed3d96d3da1e486327c5b73
Aymane Dara Hlamnach <dara-@outlook.jp>
2023-10-16 16:08:21 +0100

    dark: constants fix

--- packages/app/lib/util/theme-command.ts
+++ packages/app/lib/util/theme-command.ts
@@ -1,8 +1,5 @@
 import { Cli } from '@youcan/cli-kit';
-import { CONFIGURATION_FILE_NAME } from '@/constants';
 
 export abstract class AppCommand extends Cli.Command {
-  protected configFileName() {
-    return CONFIGURATION_FILE_NAME;
-  }
+
 }
