0d48d3d72e70e08d2fd14582a452b7a116de4ff0
Aymane Dara Hlamnach <dara-@outlook.jp>
2023-12-09 11:21:29 +0100

    install app

---
+++ packages/app/lib/cli/commands/app/install.ts
@@ -0,0 +1,33 @@
+import { Env, Http, Session, System, Tasks } from '@youcan/cli-kit';
+import { load } from '@/util/app-loader';
+import { AppCommand } from '@/util/theme-command';
+
+export default class Install extends AppCommand {
+  async run() {
+    const app = await load();
+
+    if (!app.config.id) {
+      this.output.error('Please run the `dev` command before installing your app.');
+    }
+
+    await Session.authenticate(this);
+
+    const { url } = await Tasks.run(
+      { } as { url?: string },
+      [
+        {
+          title: 'Building authorization url',
+          async task(ctx) {
+            const { url } = await Http.get<{ url: string }>(`${Env.apiHostname()}/apps/draft/${app.config.id}/authorization-url`);
+
+            ctx.url = url;
+          },
+        },
+      ],
+    );
+
+    await this.output.anykey('Press any key to open the installation page in your browser');
+
+    System.open(url!);
+  }
+}
