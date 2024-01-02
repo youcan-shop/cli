9cee600d3fe3853ac00a6ffa9a855f93ef46443d
Aymane Dara Hlamnach <dara-@outlook.jp>
2024-01-02 17:34:50 +0100

    restore old auth

--- packages/legacy-cli/lib/cli/commands/auth/login.ts
+++ packages/legacy-cli/lib/cli/commands/auth/login.ts
@@ -1,16 +1,75 @@
+import type { IncomingMessage, ServerResponse } from 'http';
+import { createServer } from 'http';
 import { exit } from 'process';
 import prompts from 'prompts';
-import type { PromptObject } from 'prompts';
+import type { TokenResponse } from './types';
 import type { CLI, CommandDefinition } from '@/cli/commands/types';
 import config from '@/config';
+import openLink from '@/utils/system/openLink';
 import stdout from '@/utils/system/stdout';
+import { post } from '@/utils/http';
 import writeToFile from '@/utils/system/writeToFile';
 import messages from '@/config/messages';
 import { getPidByPort, isPortAvailable } from '@/utils/network';
 import { kill } from '@/utils/system';
 import { LoadingSpinner } from '@/utils/common';
 
-export default function command(cli: CLI): CommandDefinition {
+/**
+ * Spin up a local server to handle the OAuth redirect. Times out after 10 seconds.
+ * @returns A promise that resolves when the code is received.
+*/
+function captureAuthorization(): Promise<string> {
+  return new Promise((resolve, reject) => {
+    let timeOut: ReturnType<typeof setTimeout>;
+
+    const server = createServer((req: IncomingMessage, res: ServerResponse) => {
+      const { url } = req;
+      const code = url?.split('=')[1] || '';
+
+      if (!code) {
+        reject(new Error('authorization code is required.'));
+      }
+
+      res.end('You can close this window now.');
+      clearTimeout(timeOut);
+
+      server.close();
+      resolve(code);
+    });
+
+    server.listen(config.OAUTH_CALLBACK_PORT, () => {
+      timeOut = setTimeout(() => {
+        reject(new Error('Timeout'));
+      }, config.OAUTH_CALLBACK_SERVER_TIMEOUT);
+    });
+  });
+}
+
+/**
+ * Exchange code for access token
+ * @param authorizationCode string
+ */
+async function exchangeAuthCode(authorizationCode: string): Promise<string> {
+  const formParams: Record<string, any> = {
+    grant_type: 'authorization_code',
+    client_id: config.OAUTH_CLIENT_ID,
+    client_secret: config.OAUTH_CLIENT_SECRET,
+    redirect_uri: config.OAUTH_CALLBACK_URL,
+    code: authorizationCode,
+  };
+
+  const res = await post<TokenResponse>(config.OAUTH_ACCESS_TOKEN_URL, {
+    method: 'POST',
+    body: new URLSearchParams(formParams),
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  });
+
+  return res.access_token;
+}
+
+export default function command(_cli: CLI): CommandDefinition {
   return {
     name: 'login',
     group: 'auth',
@@ -47,64 +106,24 @@ export default function command(cli: CLI): CommandDefinition {
         }
       }
 
-      const loading = new LoadingSpinner('Authenticating...');
-
       try {
-        const inquiries: PromptObject[] = [
-          {
-            type: 'text',
-            name: 'email',
-            message: 'Type your email',
-            validate: value => value !== '',
-          },
-          {
-            type: 'password',
-            name: 'password',
-            message: 'Type your password',
-            validate: value => value !== '',
-          },
-        ];
+        openLink(config.OAUTH_AUTH_CODE_URL);
+      }
+      catch (err) {
+        stdout.log(messages.LOGIN_OPEN_LINK);
+        stdout.info(config.OAUTH_AUTH_CODE_URL);
+      }
 
-        const credentials = await prompts(inquiries);
-
-        loading.start();
-        const response = await cli.client.auth({ email: credentials.email, password: credentials.password });
-        let token = response.token;
-
-        loading.stop();
-
-        cli.client.setAccessToken(response.token);
-
-        if (response.stores.length > 1) {
-          const choices = response.stores.map(store => ({
-            title: store.slug,
-            value: store.store_id,
-          }));
-
-          const { storeId } = await prompts({
-            type: 'select',
-            name: 'storeId',
-            message: messages.SELECT_STORE,
-            choices,
-          });
-
-          const selectStoreResponse = await cli.client.selectStore({ id: storeId });
-
-          token = selectStoreResponse.token;
-        }
+      await LoadingSpinner.exec('Authenticating..', async () => {
+        const authorization = await captureAuthorization();
 
         writeToFile(
           config.CLI_GLOBAL_CONFIG_PATH,
-          JSON.stringify({ access_token: token }),
+          JSON.stringify({ access_token: await exchangeAuthCode(authorization) }),
         );
+      });
 
-        stdout.info(messages.LOGIN_SUCCESS);
-      }
-      catch (err: any) {
-        const error = JSON.parse(err.message);
-
-        loading.error(error.detail);
-      }
+      stdout.info(messages.LOGIN_SUCCESS);
     },
   };
 }
